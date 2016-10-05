b1bccf39441d32cc8ecf594499e2826d8ab93c86
tuckyeah <h.rosebrock@gmail.com>
2016-10-04 21:44:30 -0400

    grunt build one more time

--- bundle.js
+++ bundle.js
@@ -234,8 +234,8 @@ webpackJsonp([0],[
 	'use strict';
 
 	var app = {
-	  // host: 'https://fast-citadel-82471.herokuapp.com'
-	  host: 'http://localhost:3000'
+	  host: 'https://fast-citadel-82471.herokuapp.com'
+	  // host: 'http://localhost:3000'
 	};
 
 	module.exports = app;
