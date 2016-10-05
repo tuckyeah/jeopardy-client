616e8fa8e060f3ff9192b6907933bc7966d5aada
tuckyeah <h.rosebrock@gmail.com>
2016-10-04 21:44:06 -0400

    switching host to heroku server

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const app = {
-  // host: 'https://fast-citadel-82471.herokuapp.com'
-  host: 'http://localhost:3000'
+  host: 'https://fast-citadel-82471.herokuapp.com'
+  // host: 'http://localhost:3000'
 };
 
 module.exports = app;
